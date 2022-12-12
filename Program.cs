27a15f40608530db95655f10f914b2b86a863ae4
Aman-ABJIMA <119652332+Aman-ABJIMA@users.noreply.github.com>
2022-12-12 17:40:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace BethanysPieShopHRM.HR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Employee bethany = new StoreManager(10009,"Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25);
+            Manager mary = new Manager(10008,"Mary", "Jones", "mary@snowball.be", new DateTime(1965, 1, 16), 30);
+            JuniorResearcher bobJunior = new JuniorResearcher(10007,"Bob", "Spencer", "bob@snowball.be", new DateTime(1988, 1, 23), 17);
+            Employee kevin = new StoreManager(10006,"Kevin", "Marks", "kevin@snowball.be", new DateTime(1953, 12, 12), 10);
+            Employee kate = new StoreManager(10005,"Kate", "Greggs", "kate@snowball.be", new DateTime(1993, 8, 8), 10);
+
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(bethany);
+            employees.Add(kevin);
+            employees.Add(kate);
+            employees.Add(bobJunior);
+            employees.Add(kate);
+
+            employees.Sort();
+
+            foreach (var employee in employees)
+            {
+                employee.PerformWork();
+                employee.ReceiveWage();
+                employee.DisplayEmployeeDetails();
+                employee.GiveBonus();
+                //employee.AttendManagementMeeting();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Researcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BethanysPieShopHRM.HR
+{
+    public class Researcher : Employee
+    {
+        public Researcher(int id,string first, string last, string em, DateTime bd, double? rate) : base(id, first, last, em, bd, rate)
+        {
+        }
+
+        public void ResearchNewPieTastes(int researchHours)
+        {
+            NumberOfHoursWorked += researchHours;
+            Console.WriteLine($"Researcher {FirstName} {LastName} is has invented a new pie taste!");
+        }
+
+        public double ReceiveWage()
+        {
+            double wageBeforeTax = NumberOfHoursWorked * HourlyRate.Value;
+            double taxAmount = wageBeforeTax * taxRate;
+
+            Wage = wageBeforeTax - taxAmount;
+
+            Console.WriteLine($"The wage for {NumberOfHoursWorked} hours of work is {Wage}.");
+            NumberOfHoursWorked = 0;
+
+            return Wage;
+        }
+    }
+
+    public class JuniorResearcher : Researcher
+    {
+        public JuniorResearcher(int id,string first, string last, string em, DateTime bd, double? rate) : base(id,first, last, em, bd, rate)
+        {
+        }
+
+        public double ReceiveWage()
+        {
+            double wageBeforeTax = NumberOfHoursWorked * HourlyRate.Value;
+            double taxAmount = wageBeforeTax * taxRate;
+
+            Wage = wageBeforeTax - taxAmount;
+
+            Console.WriteLine($"The wage for {NumberOfHoursWorked} hours of work is {Wage}.");
+            NumberOfHoursWorked = 0;
+
+            return Wage;
+        }
+    }
+}
